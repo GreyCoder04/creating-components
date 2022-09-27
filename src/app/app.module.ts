0bf830e55a1f26b510c47d830b5fc3328d02ce05
GreyCoder04 <100299497+GreyCoder04@users.noreply.github.com>
2022-09-27 14:23:45 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, PostComponent, ParagraphComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/paragraph/paragraph.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-para",
+  templateUrl: "paragraph.component.html",
+  styleUrls: ['./paragraph.component.css']
+})
+export class ParagraphComponent {}
